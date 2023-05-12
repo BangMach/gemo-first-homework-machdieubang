b87bc1efeb5b4739f8900a4181e7e6b1d1c1ef5d
Bang MachDieu <bang.machdieu@alfalaval.com>
2023-05-12 16:40:40 +0700

    add implementaion for solution 3

--- calculatePrice3.py
+++ calculatePrice3.py
@@ -1,3 +1,8 @@
+# Add function calculatePrice3 which allow customization options for chocolate sauce, which can only be added to hot drinks and has the following pricing:
+# The first 2 pumps are free
+# $0.5 for each extra pump
+# Maximum of 6 pumps
+
 def calculatePrice2(order):
     # Define the price dictionary
     priceDict = {
@@ -13,26 +18,39 @@ def calculatePrice2(order):
         "BasedCoffee": 2,
         "BasedMilktea": 2.25,
         "AlmondMilk": 0.5,
-        "WholeMilk": 0.5,
-        "chocolatePump":0.5
+        "WholeMilk": 0,
+        "Chocolate Sauce":0.5  
     }
     
     # Initialize base price to 0
     basePrice = 0
+    num_of_chocolate_pump =0
     
+    #check based drink
+    if "BasedMilktea" in order:
+        basePrice = priceDict["BasedMilktea"]
+    elif "BasedCoffee" in order:
+        basePrice = priceDict["BasedCoffee"]
+
+
     # Check for drink type and adjust base price
     if "Hot" in order:
-        basePrice = priceDict["BasedCoffee"]
+        basePrice = basePrice + priceDict["Hot"]
+        if "Chocolate Sauce" in order:
+            for item in order:
+                if item == "Chocolate Sauce":
+                    num_of_chocolate_pump+=1
+                    if num_of_chocolate_pump <= 2:
+                        continue
+                    elif num_of_chocolate_pump <= 6:
+                        basePrice += priceDict["Chocolate Sauce"]
+                    else:
+                        break 
     elif "Cold" in order:
-        basePrice = priceDict["BasedCoffee"]
-        if "L" in order:
-            basePrice += priceDict["L"]
+        basePrice = basePrice + priceDict["Cold"]
     elif "Blended" in order:
-        basePrice = priceDict["BasedCoffee"] + priceDict["Blended"]
-        if "L" in order:
-            basePrice += priceDict["L"]
-    elif "BasedMilktea" in order:
-        basePrice = priceDict["BasedMilktea"]
+        basePrice = basePrice + priceDict["Blended"]
+        
     
     # Check for size and price adjustment
     if "S" in order:
@@ -44,8 +62,9 @@ def calculatePrice2(order):
     elif "XL" in order:
         sizePrice = priceDict["XL"]
     else:
-        sizePrice = 0
-    
+        print("Size L only available for Cold and Hot Drink")
+        return 0    
+        
     # Check for whipped cream topping
     if "WithoutWhippedCream" in order:
         creamPrice = priceDict["WithoutWhippedCream"]
@@ -67,7 +86,36 @@ def calculatePrice2(order):
     return totalPrice
     
     
-print(calculatePrice2(["Hot", "M", "WhippedCream"])) # Output should be 2.5
-print(calculatePrice2(["Cold", "L", "WithoutWhippedCream"])) # Output should be 3.0
-print(calculatePrice2(["Blended", "XL", "AlmondMilk", "WithCreamTopping"])) # Output should be 6.0
-print(calculatePrice2(["BasedMilktea", "S", "WholeMilk"])) # Output should be 2.75
+# print(calculatePrice2(["Hot", "M", "WhippedCream"])) # Output should be 2.5
+# print(calculatePrice2(["Cold", "L", "WithoutWhippedCream"])) # Output should be 3.0
+# print(calculatePrice2(["Blended", "XL", "AlmondMilk", "WithCreamTopping"])) # Output should be 6.0
+# print(calculatePrice2(["BasedMilktea", "S", "WholeMilk"])) # Output should be 2.75
+# print(calculatePrice2(["Hot", "S", "WithoutWhippedCream"])) # Output should be 2.0
+# print(calculatePrice2(["Hot", "S", "WhippedCream"])) # Output should be 2.5
+# print(calculatePrice2(["Hot", "M", "WithoutWhippedCream"])) # Output should be 2.0
+# print(calculatePrice2(["Hot", "M", "WhippedCream"])) # Output should be 2.5
+
+# from previous tests
+# print(calculatePrice2(["BasedCoffee","Hot", "S", "WithoutWhippedCream"])) # Output should be 2.0
+# print(calculatePrice2(["BasedCoffee","Hot", "S", "WhippedCream"])) # Output should be 2.5
+# print(calculatePrice2(["BasedCoffee","Hot", "M", "WithoutWhippedCream"])) # Output should be 2.5
+# print(calculatePrice2(["BasedCoffee","Hot", "M", "WhippedCream"])) # Output should be 3.0
+# print(calculatePrice2(["BasedCoffee","L", "Blended", "WithoutWhippedCream"])) # Output should be 4.00
+# print(calculatePrice2(["BasedCoffee","L", "Blended", "WhippedCream"])) # Output should be 4.50
+# print(calculatePrice2(["BasedCoffee","L", "Cold", "WithoutWhippedCream"])) # Output should be 3.00
+# print(calculatePrice2(["BasedCoffee","L", "Cold", "WhippedCream"])) # Output should be 3.50
+# print(calculatePrice2(["BasedCoffee","L", "Hot", "WithoutWhippedCream"])) # Should be an error" Size L only available for Cold and Hot Drink"
+# print(calculatePrice2(["BasedCoffee","M", "Blended", "WithoutWhippedCream"])) # Output should be 3.50
+
+
+# new tests
+print(calculatePrice2(["BasedMilktea","Hot", "S", "WithoutWhippedCream","Chocolate Sauce","Chocolate Sauce"])) # Output should be 2.25
+print(calculatePrice2(["BasedMilktea","Hot", "S", "WithoutWhippedCream","Chocolate Sauce","Chocolate Sauce","Chocolate Sauce","Chocolate Sauce"])) # Output should be 3.25
+print(calculatePrice2(["BasedMilktea","Hot", "M", "WithoutWhippedCream","AlmondMilk"])) # Output should be 3.25
+print(calculatePrice2(["BasedMilktea","Hot", "M", "WithoutWhippedCream","WholeMilk"])) # Output should be 2.75
+print(calculatePrice2(["BasedMilktea","XL", "Blended", "WithoutWhippedCream"])) # Output should be 4.75
+print(calculatePrice2(["BasedMilktea","L", "Blended", "WhippedCream"])) # Output should be 4.75
+print(calculatePrice2(["BasedMilktea","L", "Cold", "WithoutWhippedCream"])) # Output should be 3.25
+print(calculatePrice2(["BasedMilktea","L", "Cold", "WhippedCream"])) # Output should be 3.75
+print(calculatePrice2(["BasedMilktea","L", "Hot", "WithoutWhippedCream"])) # Should be an error" Size L only available for Cold and Hot Drink"
+print(calculatePrice2(["BasedMilktea","M", "Blended", "WithoutWhippedCream"])) # Output should be 3.75
